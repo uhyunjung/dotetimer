6b780a44cde9bba0fe81db688f46871dbd62b0ac
yhyeonjg <yhyeonjg@naver.com>
2023-03-21 21:30:59 +0900

    Update 3/21

--- src/main/java/com/dotetimer/infra/Green.java
+++ src/main/java/com/dotetimer/infra/Green.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum Green { // 하루 코인 개수 0 0~2 2~6 6~10 10+ / 총 코인 개수 0 0~12 12~36 36~60 60+
+public enum Green { // 하루 코인 개수 0 0~2 2~6 6~10 10+, 총 코인 개수 0 0~12 12~36 36~60 60+
     LEVEL_ZERO(0, "#F1F8E9"), // 초록색 채도 50 200 400 600 900
     LEVEL_ONE(2, "#AED581"),
     LEVEL_TWO(6, "#9CCC65"),
